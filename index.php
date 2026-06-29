c5c09370d2c33f4b5d944dbe04725ccf7bd8bb72
priyanshi950791-lab <priyanshi950791@gmail.com>
2026-06-29 16:08:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,46 @@
+<!--priyanshi950791@email.com -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mini API</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Mini API Project</h2>
+
+<form method="GET">
+    <input type="text" name="name" placeholder="Enter your name">
+    <button type="submit">Get Greeting</button>
+</form>
+
 <?php
-
-header("Content-Type: application/json");
-
-$name = "Guest";
-
-if(isset($_GET["name"]))
+if(isset($_GET['name']))
 {
-    $name = $_GET["name"];
+    $name = htmlspecialchars($_GET['name']);
+
+    try
+    {
+        $url = "http://localhost/mini-api/api/greet.php?name=".$name;
+
+        $json = file_get_contents($url);
+
+        if($json === FALSE)
+        {
+            throw new Exception("API Error");
+        }
+
+        $data = json_decode($json, true);
+
+        echo "<h3>".$data["message"]."</h3>";
+    }
+    catch(Exception $e)
+    {
+        echo "<p>Something went wrong.</p>";
+    }
 }
-
-echo json_encode([
-    "message" => "Hello, ".$name."!"
-]);
-
 ?>
+
+</body>
+</html>
